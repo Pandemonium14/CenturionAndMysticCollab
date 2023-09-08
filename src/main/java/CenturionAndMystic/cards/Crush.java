198abaa5312cb875b03676b95aac63ea83ce6725
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-09-08 16:11:08 -0300

    Add Crush, MaceCrash

---
+++ src/main/java/CenturionAndMystic/cards/Crush.java
@@ -0,0 +1,51 @@
+package CenturionAndMystic.cards;
+
+import CenturionAndMystic.cards.abstracts.AbstractCenturionCard;
+import CenturionAndMystic.patches.CustomTags;
+import CenturionAndMystic.util.Wiz;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.red.BodySlam;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.VerticalImpactEffect;
+
+import static CenturionAndMystic.MainModfile.makeID;
+
+public class Crush extends AbstractCenturionCard {
+    public final static String ID = makeID(Crush.class.getSimpleName());
+
+    public Crush() {
+        super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = damage = 10;
+        tags.add(CustomTags.CAM_BASH_EFFECT);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (!Wiz.hasInfusion() && m != null) {
+            addToBot(new VFXAction(new VerticalImpactEffect(m.hb.cX + m.hb.width / 4.0F, m.hb.cY - m.hb.height / 4.0F)));
+            dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
+        }
+        dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
+    }
+
+    public void triggerOnGlowCheck() {
+        if (!Wiz.hasInfusion()) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        } else {
+            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        }
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(2);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return BodySlam.ID;
+    }
+}

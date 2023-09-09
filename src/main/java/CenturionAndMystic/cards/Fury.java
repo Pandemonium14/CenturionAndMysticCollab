8bb2b91c882912110fe6d10386b4e0fc61fe8aa6
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-09-08 21:19:01 -0300

    Add Fury

---
+++ src/main/java/CenturionAndMystic/cards/Fury.java
@@ -0,0 +1,38 @@
+package CenturionAndMystic.cards;
+
+import CenturionAndMystic.cards.abstracts.AbstractCenturionCard;
+import CenturionAndMystic.patches.CustomTags;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.red.Rage;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static CenturionAndMystic.MainModfile.makeID;
+
+public class Fury extends AbstractCenturionCard {
+    public final static String ID = makeID(Fury.class.getSimpleName());
+
+    public Fury() {
+        super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = damage = 4;
+        baseMagicNumber = magicNumber = 3;
+        tags.add(CustomTags.CAM_BASH_EFFECT);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0 ; i < magicNumber ; i++) {
+            dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY, true);
+        }
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(2);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Rage.ID;
+    }
+}

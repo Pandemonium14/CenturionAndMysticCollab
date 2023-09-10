27c18858b62406bac8f3a7296f174895fb2cbf65
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-09-10 19:23:44 -0300

    Add new param options for ShedAction

--- src/main/java/CenturionAndMystic/actions/ShedAction.java
+++ src/main/java/CenturionAndMystic/actions/ShedAction.java
@@ -5,15 +5,28 @@ import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.ExhaustAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class ShedAction extends BetterSelectCardsInHandAction {
     public ShedAction(int amount) {
-        super(amount, ExhaustAction.TEXT[0], false, false, c -> true, l -> {
+        this(amount, c -> true, l -> {});
+    }
+
+    public ShedAction(int amount, Predicate<AbstractCard> filter) {
+        this(amount, filter, l -> {});
+    }
+
+    public ShedAction(int amount, Predicate<AbstractCard> filter, Consumer<List<AbstractCard>> callback) {
+        super(amount, ExhaustAction.TEXT[0], false, false, filter, l -> {
             for (AbstractCard c : l) {
                 Wiz.adp().hand.moveToExhaustPile(c);
             }
             if (!l.isEmpty()) {
                 Wiz.att(new DrawCardAction(l.size()));
             }
+            callback.accept(l);
             l.clear();
         });
     }

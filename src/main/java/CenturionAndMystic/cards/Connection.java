1a4d439aba10a56e561d412264110909b95bf7e1
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-09-13 10:03:40 -0300

    Connection now unplayable and not Centurion/Mystic

--- src/main/java/CenturionAndMystic/cards/Connection.java
+++ src/main/java/CenturionAndMystic/cards/Connection.java
@@ -1,7 +1,7 @@
 package CenturionAndMystic.cards;
 
 import CenturionAndMystic.actions.ModifyCustomEnergyAction;
-import CenturionAndMystic.cards.abstracts.AbstractMysticCard;
+import CenturionAndMystic.cards.abstracts.AbstractEasyCard;
 import CenturionAndMystic.patches.CustomTags;
 import CenturionAndMystic.util.Wiz;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
@@ -11,25 +11,33 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static CenturionAndMystic.MainModfile.makeID;
 
-public class Connection extends AbstractMysticCard {
+public class Connection extends AbstractEasyCard {
     public final static String ID = makeID(Connection.class.getSimpleName());
 
     public Connection() {
-        super(ID, 0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        super(ID, -2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
         tags.add(CustomTags.CAM_MAGIC_EFFECT);
-        exhaust = true;
+        isEthereal = true;
     }
 
     @Override
-    public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ModifyCustomEnergyAction(-Wiz.centurionEnergy(), ModifyCustomEnergyAction.EnergyType.CENTURION));
-        addToBot(new ModifyCustomEnergyAction(-Wiz.mysticEnergy(), ModifyCustomEnergyAction.EnergyType.MYSTIC));
-        addToBot(new GainEnergyAction(Wiz.mysticEnergy()+Wiz.centurionEnergy()));
+    public void use(AbstractPlayer p, AbstractMonster m) {}
+
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
+        return false;
+    }
+
+    @Override
+    public void triggerWhenDrawn() {
+        addToTop(new GainEnergyAction(Wiz.mysticEnergy()+Wiz.centurionEnergy()));
+        addToTop(new ModifyCustomEnergyAction(-Wiz.mysticEnergy(), ModifyCustomEnergyAction.EnergyType.MYSTIC));
+        addToTop(new ModifyCustomEnergyAction(-Wiz.centurionEnergy(), ModifyCustomEnergyAction.EnergyType.CENTURION));
     }
 
     @Override
     public void upp() {
-        exhaust = false;
+        isEthereal = false;
         uDesc();
     }
 

2d519cbe2bd8695b92eb2115411a4fe8d26837de
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-09-15 18:34:33 -0300

    Add Bless back as Common

--- src/main/java/CenturionAndMystic/cards/Bless.java
+++ src/main/java/CenturionAndMystic/cards/Bless.java
@@ -1,9 +1,10 @@
-package CenturionAndMystic.cutStuff;
+package CenturionAndMystic.cards;
 
 import CenturionAndMystic.cards.abstracts.AbstractMysticCard;
 import CenturionAndMystic.patches.CustomTags;
-import CenturionAndMystic.powers.InfuseHexPower;
+import CenturionAndMystic.powers.InfuseMightPower;
 import CenturionAndMystic.util.Wiz;
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
 import com.megacrit.cardcrawl.cards.colorless.Enlightenment;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -14,19 +15,20 @@ public class Bless extends AbstractMysticCard {
     public final static String ID = makeID(Bless.class.getSimpleName());
 
     public Bless() {
-        super(ID, 1, CardType.SKILL, CardRarity.BASIC, CardTarget.SELF);
-        baseMagicNumber = magicNumber = 2;
+        super(ID, 2, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
+        baseMagicNumber = magicNumber = 4;
         tags.add(CustomTags.CAM_MAGIC_EFFECT);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.applyToSelf(new InfuseHexPower(p, magicNumber));
+        Wiz.applyToSelf(new InfuseMightPower(p, magicNumber));
+        addToBot(new AddTemporaryHPAction(p, p, magicNumber));
     }
 
     @Override
     public void upp() {
-        upgradeMagicNumber(1);
+        upgradeMagicNumber(2);
     }
 
     @Override

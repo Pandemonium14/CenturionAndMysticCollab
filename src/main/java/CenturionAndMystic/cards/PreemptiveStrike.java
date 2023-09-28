fde7e257223812c1f310ed142e8fb285099de41a
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-09-28 09:46:20 -0300

    Rework PreemptiveStrike

--- src/main/java/CenturionAndMystic/cards/PreemptiveStrike.java
+++ src/main/java/CenturionAndMystic/cards/PreemptiveStrike.java
@@ -3,11 +3,10 @@ package CenturionAndMystic.cards;
 import CenturionAndMystic.cards.abstracts.AbstractMysticCard;
 import CenturionAndMystic.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.green.Outmaneuver;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static CenturionAndMystic.MainModfile.makeID;
 
@@ -15,9 +14,8 @@ public class PreemptiveStrike extends AbstractMysticCard {
     public final static String ID = makeID(PreemptiveStrike.class.getSimpleName());
 
     public PreemptiveStrike() {
-        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
-        baseDamage = damage = 6;
-        baseMagicNumber = magicNumber = 1;
+        super(ID, 0, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = damage = 8;
         tags.add(CardTags.STRIKE);
         exhaust = true;
         isInnate = true;
@@ -25,14 +23,31 @@ public class PreemptiveStrike extends AbstractMysticCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-        Wiz.applyToEnemy(m, new WeakPower(m, magicNumber, false));
-        Wiz.applyToEnemy(m, new VulnerablePower(m, magicNumber, false));
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_HEAVY);
     }
 
     @Override
     public void upp() {
-        upgradeMagicNumber(1);
+        upgradeDamage(3);
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        int base = this.baseDamage;
+        if (!Wiz.isAttacking(mo)) {
+            this.baseDamage *= 2;
+        }
+        super.calculateCardDamage(mo);
+        this.baseDamage = base;
+        this.isDamageModified = baseDamage != damage;
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        if (Wiz.anyMonsterNotAttacking()) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        }
     }
 
     @Override

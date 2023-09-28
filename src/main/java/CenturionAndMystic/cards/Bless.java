2727f7c137f72f8121f44855cb0df2d1f5bd2f6d
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-09-28 09:14:29 -0300

    Rework Bless

--- src/main/java/CenturionAndMystic/cards/Bless.java
+++ src/main/java/CenturionAndMystic/cards/Bless.java
@@ -2,8 +2,6 @@ package CenturionAndMystic.cards;
 
 import CenturionAndMystic.cards.abstracts.AbstractMysticCard;
 import CenturionAndMystic.patches.CustomTags;
-import CenturionAndMystic.powers.InfuseMightPower;
-import CenturionAndMystic.util.Wiz;
 import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
 import com.megacrit.cardcrawl.cards.colorless.Enlightenment;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -16,19 +14,18 @@ public class Bless extends AbstractMysticCard {
 
     public Bless() {
         super(ID, 2, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
-        baseMagicNumber = magicNumber = 4;
+        baseMagicNumber = magicNumber = 8;
         tags.add(CustomTags.CAM_MAGIC_EFFECT);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.applyToSelf(new InfuseMightPower(p, magicNumber));
         addToBot(new AddTemporaryHPAction(p, p, magicNumber));
     }
 
     @Override
     public void upp() {
-        upgradeMagicNumber(2);
+        upgradeMagicNumber(3);
     }
 
     @Override

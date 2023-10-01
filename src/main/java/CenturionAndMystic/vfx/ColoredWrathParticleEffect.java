c2bc71765d59d87098cd8043fc99ca2af10fe2a8
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-10-01 11:49:41 -0300

    Fix wierd Infusion VFX blinking due to not setting alpha correctly

--- src/main/java/CenturionAndMystic/vfx/ColoredWrathParticleEffect.java
+++ src/main/java/CenturionAndMystic/vfx/ColoredWrathParticleEffect.java
@@ -6,6 +6,6 @@ import com.megacrit.cardcrawl.vfx.stance.WrathParticleEffect;
 public class ColoredWrathParticleEffect extends WrathParticleEffect {
     public ColoredWrathParticleEffect(Color c) {
         super();
-        this.color.set(c);
+        this.color.set(c.r, c.g, c.b, 0f);
     }
 }

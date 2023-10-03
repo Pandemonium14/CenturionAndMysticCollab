9cd3488d2eca0570d09534a187b6ffc864827020
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-10-03 10:48:25 -0300

    Cut QuickThinking

--- src/main/java/CenturionAndMystic/cutStuff/QuickThinking.java
+++ src/main/java/CenturionAndMystic/cutStuff/QuickThinking.java
@@ -1,4 +1,4 @@
-package CenturionAndMystic.cards;
+package CenturionAndMystic.cutStuff;
 
 import CenturionAndMystic.cards.abstracts.AbstractCenturionCard;
 import CenturionAndMystic.powers.QuickThinkingPower;

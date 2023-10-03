e6f2f0ff748952885ba8066f18961149ff759426
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-10-03 19:12:25 -0300

    Rework Crush.

--- src/main/java/CenturionAndMystic/cards/Crush.java
+++ src/main/java/CenturionAndMystic/cards/Crush.java
@@ -2,14 +2,10 @@ package CenturionAndMystic.cards;
 
 import CenturionAndMystic.cards.abstracts.AbstractCenturionCard;
 import CenturionAndMystic.patches.CustomTags;
-import CenturionAndMystic.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.red.BodySlam;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.vfx.combat.VerticalImpactEffect;
 
 import static CenturionAndMystic.MainModfile.makeID;
 
@@ -18,31 +14,27 @@ public class Crush extends AbstractCenturionCard {
 
     public Crush() {
         super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
-        baseDamage = damage = 10;
+        baseDamage = damage = 16;
         tags.add(CustomTags.CAM_BASH_EFFECT);
-        //CardModifierManager.addModifier(this, new PoisedMod(true));
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (!Wiz.hasInfusion() && m != null) {
-            addToBot(new VFXAction(new VerticalImpactEffect(m.hb.cX + m.hb.width / 4.0F, m.hb.cY - m.hb.height / 4.0F)));
-            dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
-        }
         dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
     }
 
-    public void triggerOnGlowCheck() {
-        if (!Wiz.hasInfusion()) {
-            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
-        } else {
-            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
-        }
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        int base = baseDamage;
+        baseDamage += mo.currentBlock;
+        super.calculateCardDamage(mo);
+        baseDamage = base;
+        isDamageModified = baseDamage != damage;
     }
 
     @Override
     public void upp() {
-        upgradeDamage(2);
+        upgradeDamage(6);
     }
 
     @Override
